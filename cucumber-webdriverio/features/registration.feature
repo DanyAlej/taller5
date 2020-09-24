3252b9038d4901ecd204d778798396e0dd1394dc
DanyAlej <da.benavides@uniandes.edu.co>
2020-09-23 19:08:31 -0500

    Cucumber done

---
+++ cucumber-webdriverio/features/registration.feature
@@ -0,0 +1,42 @@
+Feature: Registering into losestudiantes
+    As an user I want to create an account in losestudiantes
+
+Scenario Outline: Registration failed with existing email
+
+  Given I go to losestudiantes home screen
+    When I open the 'ingresar' screen
+    And Put input with <name> with <lastname> with <email> with <university> with <maestria> with <undergrad> with <password> and <conditions>
+    And I try to register
+    Then I expect to see popup <error>
+
+    Examples:
+      | name             | lastname | email             | university               | maestria | undergrad      | password   | conditions | error    | 
+      | dany             | dany     | dany@gmail.com    | Universidad de los Andes | no       | Administración | dany123456 | yes        | "Error: Ya existe un usuario"  |
+
+Scenario Outline: Registration failed password too short 
+
+  Given I go to losestudiantes home screen
+    When I open the 'ingresar' screen
+    And Put input with <name> with <lastname> with <email> with <university> with <maestria> with <undergrad> with <password> and <conditions>
+    And I try to register
+    Then I expect to see <error>
+
+    Examples:
+      | name             | lastname | email             | university               | maestria | undergrad      | password   | conditions | error    |
+      | dany             | dany     | dany@gmail.com    | Universidad de los Andes | no       | Administración | day6       | yes        | "debe ser al menos"  |
+
+
+Scenario Outline: Registration failed empty fields 
+
+  Given I go to losestudiantes home screen
+    When I open the 'ingresar' screen
+    And Put input with <name> with <lastname> with <email> with <university> with <maestria> with <undergrad> with <password> and <conditions>
+    And I try to register
+    Then I expect to see <error>
+
+    Examples:
+      | name             | lastname | email             | university               | maestria | undergrad      | password   | conditions | error    |
+      | dany             | dany     |                   | Universidad de los Andes | no       | Administración | dfdsfkjsf6 | yes        | "Ingresa tu correo"  |
+      | dany             | dany     | dany@gmail.com    | Universidad de los Andes | no       | Administración |            | yes        | "Ingresa una contr"  |
+      | dany             | dany     | dany@gmail.com    | Universidad de los Andes | no       | Administración | danlsdfy6  |            | "Debes aceptar"  |
+
--- cucumber-webdriverio/features/step-definitions/index.js
+++ cucumber-webdriverio/features/step-definitions/index.js
@@ -2,6 +2,7 @@ var {Given} = require('cucumber');
 var {When} = require('cucumber');
 var {Then} = require('cucumber');
 var {expect} = require('chai');
+const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
 
 Given('I go to losestudiantes home screen', () => {
   browser.url('/');
@@ -50,12 +51,85 @@ When(/^I fill with (.*) and (.*)$/ , (email, password) => {
  passwordInput.keys(password)
 });
 
-Then('I expect to see {string}', error => {
- $('.aviso.alert.alert-danger').waitForDisplayed(5000);
- var alertText = browser.$('.aviso.alert.alert-danger').getText();
- expect(alertText).to.include(error);
+Then('I expect to see popup {string}', (error) => {
+    $('div[class="text-muted lead"]').waitForExist(5000);
+    var h2Text = browser.$('div[class="text-muted lead"]').getText();
+    expect(h2Text).to.include(error);
 });
 
 Then('I expect to see the account avatar', () => {
-  var divCuenta = browser.$('.cuenta');
+  browser.$('.cuenta');
 });
+
+Then('I expect to see {string}', error => {
+    $('.aviso.alert.alert-danger').waitForDisplayed(5000);
+    var alertText = browser.$('.aviso.alert.alert-danger').getText();
+    expect(alertText).to.include(error);
+});
+
+
+// REGISTRATION
+
+Then('I expect pop up {string}', (error) => {
+ $('.text-muted lead').waitForDisplayed(5000);
+ var alertText = browser.$('text-muted lead').getText();
+ expect(alertText).to.include(error);
+});
+
+When("I open the 'ingresar' screen", () => {
+  $('button=Ingresar').waitForExist(5000);
+  $('button=Ingresar').waitForDisplayed(5000);
+  $('button=Ingresar').click();
+});
+
+When(/^Put input with (.*) with (.*) with (.*) with (.*) with (.*) with (.*) with (.*) and (.*)$/ , (name, lastname, email, university, maestria, undergrad, password, conditions) => {
+  var cajaSignUp = $('.cajaSignUp');
+
+  cajaSignUp.$('input[name="nombre"]').waitForExist(5000);
+  cajaSignUp.$('input[name="nombre"]').waitForDisplayed(5000);
+
+  var nameInput = cajaSignUp.$('input[name="nombre"]');
+  nameInput.click();
+  nameInput.keys(name);
+
+  var lastnameInput = cajaSignUp.$('input[name="apellido"]');
+  lastnameInput.click();
+  lastnameInput.keys(lastname);
+
+  var mailInput = cajaSignUp.$('input[name="correo"]');
+  mailInput.click();
+  mailInput.keys(email);
+
+  var universityCombobox = cajaSignUp.$('select[name="idUniversidad"]');
+  var universidad = university.toLowerCase().split(' ').join('-');
+  var universityOption = universityCombobox.$(`option[value="${universidad}"]`);
+  universityOption.click();
+
+  if ( maestria === 'yes') {
+    var maestriaCheckbox = $('input=Estudio una maestria');
+    check(maestriaCheckbox);
+  }
+
+  var undergradCombobox = cajaSignUp.$('select[name="idPrograma"]');
+  var undergradOption = undergradCombobox.$(`option[value="22"]`);
+  undergradOption.click();
+
+  var passwordInput = cajaSignUp.$('input[name="password"]');
+  passwordInput.click();
+  passwordInput.keys(password);
+
+  if(conditions === 'yes'){
+    var checkTerms = cajaSignUp.$('input[name="acepta"]');
+    checkTerms.click();
+  }
+
+});
+
+When('I try to register', () => {
+    var cajaSignUp = $('.cajaSignUp');
+    cajaSignUp.$('button=Registrarse').click();
+
+  sleep(10000).then(() => {
+  });
+});
+
